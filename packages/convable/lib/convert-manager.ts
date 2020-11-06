b894d89f2cbbdc266184dbc766520b3670fc5f6f
siyual-park <siyual.bak@gmail.com>
2020-11-06 21:03:21 +0900

    feature/remove_meta_and_add_priotiy

--- packages/convable/lib/convert-manager.ts
+++ packages/convable/lib/convert-manager.ts
@@ -2,10 +2,10 @@ import Converter from "./converter";
 import ConvertProcessor from "./convert-processor";
 import ConvertException from "./convert-exception";
 
-export default class ConvertManager<M = undefined> implements Converter<M> {
-  private readonly processors: ConvertProcessor<unknown, unknown, M>[] = [];
+export default class ConvertManager implements Converter {
+  private readonly processors: ConvertProcessor<unknown, unknown>[] = [];
 
-  convert(value: unknown, meta?: M): unknown {
+  convert(value: unknown): unknown {
     const matchedProcessor = this.processors.find((processor) =>
       processor.isConvertible(value)
     );
@@ -13,13 +13,25 @@ export default class ConvertManager<M = undefined> implements Converter<M> {
       throw new ConvertException("Can't find processor");
     }
 
-    return matchedProcessor.convert(value, this, meta);
+    return matchedProcessor.convert(value, this);
   }
 
   register(
-    converter: ConvertProcessor<unknown, unknown, M>
-  ): ConvertManager<M> {
-    this.processors.push(converter);
+    converter: ConvertProcessor<unknown, unknown>,
+    priority = 0
+  ): ConvertManager {
+    const index = this.getIndex(priority);
+    this.processors.splice(index, 0, converter);
     return this;
+  }
+
+  unregister(priority: number): ConvertManager {
+    const index = this.getIndex(priority);
+    this.processors.splice(index, 1);
+    return this;
+  }
+
+  private getIndex(priority: number): number {
+    return priority >= 0 ? priority : this.processors.length + priority + 1;
   }
 }
